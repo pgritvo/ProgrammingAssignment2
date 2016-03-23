29670f30b8a79e087ffbdf900ca6a0dffdf73bfc
pgritvo <p.ritvo@hotmail.fr>
2016-03-23 18:35:10 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,78 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, makeCacheMatrix creates a special "vector",
 
-## Write a short comment describing this function
+## which is really a list containing a function to
+
+##
+
+## set the value of the matrix
+
+## get the value of the matrix
+
+## set the value of the inverted matrix
+
+## get the value of the inverteed matrix
+
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
+
+        sol <- NULL
+
+        set <- function(y) {
+
+                x <<- y
+
+                sol <<- NULL
+
+        }
+
+        get <- function() x
+
+        setsolved <- function(solved) sol <<- solved
+
+        getsolved <- function() sol
+
+        list(set = set, get = get,
+
+             setsolved = setsolved,
+
+             getsolved = getsolved)
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## The following function calculates the inverted matrix. However, it first checks to see if
+
+## the inverted matrix has already been calculated. If so, it gets the inverted matrix from the
+
+## cache and skips the computation. Otherwise, it calculates the inverted matrix and sets its value
+
+## in the cache via the setsolved function.
+
+
+
+
+cachesolve <- function(x, ...) {
+
+        sol <- x$getsolved()
+
+        if(!is.null(sol)) {
+
+                message("getting cached data")
+
+                return(sol)
+
+        }
+
+        data <- x$get()
+
+        sol <- solve(data, ...)
+
+        x$setsolved(sol)
+
+        sol
+
 }
